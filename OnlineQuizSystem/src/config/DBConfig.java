aa308b65da059282d1b5ac998a77b6e4fa8f84c7
Shraddha Verma <vnsvermashraddha@gmail.com>
2025-09-07 22:52:03 +0530

    Add files via upload

---
+++ OnlineQuizSystem/src/config/DBConfig.java
@@ -0,0 +1,6 @@
+package config;
+public class DBConfig {
+    public static final String URL = "jdbc:mysql://localhost:3306/online_quiz?useSSL=false&serverTimezone=UTC";
+    public static final String USER = "root"; 
+    public static final String PASS = "password"; 
+}
---
+++ OnlineQuizSystem/src/util/DBUtil.java
@@ -0,0 +1,12 @@
+package util;
+import config.DBConfig;
+import java.sql.*;
+public class DBUtil {
+    static {
+        try { Class.forName("com.mysql.cj.jdbc.Driver"); }
+        catch (ClassNotFoundException e) { e.printStackTrace(); }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DBConfig.URL, DBConfig.USER, DBConfig.PASS);
+    }
+}
